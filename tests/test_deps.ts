2df8b403e1d553ef69b0cd94b4cb60a499fd9dfa
mcgear <michael.gearhardt@gmail.com>
2023-07-11 12:32:10 -0600

    Standard library version syncing

--- tests/test_deps.ts
+++ tests/test_deps.ts
@@ -1,11 +1,11 @@
 export {
   assert,
   assertEquals,
-} from "https://deno.land/std@0.152.0/testing/asserts.ts";
+} from "$std/testing/asserts.ts";
 export {
   afterEach,
   beforeEach,
   describe,
   it,
-} from "https://deno.land/std@0.193.0/testing/bdd.ts";
+} from "$std/testing/bdd.ts";
 export { render } from "preact-render-to-string";
