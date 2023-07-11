1fb304c92dec667706fdc912aaca47977e1596e2
mcgear <michael.gearhardt@gmail.com>
2023-07-11 17:01:18 -0600

    Green

--- src/atoms/Button.tsx
+++ src/atoms/Button.tsx
@@ -4,7 +4,7 @@ export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
   return (
     <button
       {...props}
-      class="px-4 py-2 font-bold text-white bg-green-500 hover:bg-green-700 rounded transition-colors duration-200 ease-out focus:outline-none"
+      class="px-4 py-2 font-bold text-white bg-blue-500 hover:bg-blue-700 rounded transition-colors duration-200 ease-out focus:outline-none"
     />
   );
 }
