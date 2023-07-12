400641db0f7e040c1342070560a3e27321a31816
mcgear <michael.gearhardt@gmail.com>
2023-07-11 23:33:16 -0600

    Adjust header to simplify api

--- tests/organisms/Header.test.tsx
+++ tests/organisms/Header.test.tsx
@@ -12,10 +12,10 @@ import { Action } from "../../src/atoms/Action.tsx";
 import { Header } from "../../src/organisms/Header.tsx";
 
 describe("Header Tests", () => {
-  describe("Exists", () => {
+  describe("Logo Simple", () => {
     const html = render(
       <Header
-        logo={<Action>Hello World</Action>}
+        logo={<Action href="/">Hello World</Action>}
         nav={
           <>
             <Action href="/" class="text-xl mx-1">
@@ -35,6 +35,38 @@ describe("Header Tests", () => {
     );
 
     assert(html.includes("Hello World"));
+    assert(html.includes("Contact"));
+  });
+
+  describe("Logo Children", () => {
+    const html = render(
+      <Header
+        logo={{
+          LogoHref: "/",
+          LogoUrl: "http://localhost:8000/logo.svg",
+          LogoAlt: "Fathym Open BioTech",
+        }}
+        nav={
+          <>
+            <Action href="/" class="text-xl mx-1">
+              Home
+            </Action>
+
+            <Action href="/about" class="text-xl mx-1">
+              About
+            </Action>
+
+            <Action href="/contact" class="text-xl mx-1">
+              Contact
+            </Action>
+          </>
+        }
+      />,
+    );
+
+    assert(html.includes('href="/"'));
+    assert(html.includes("http://localhost:8000/logo.svg"));
+    assert(html.includes("Fathym Open BioTech"));
     assert(html.includes("Contact"));
   });
 });
