428f0cf0f78b91911404f2ba2cb647d67168caab
mcgear <michael.gearhardt@gmail.com>
2023-07-12 10:34:49 -0600

    Footer

--- src/organisms/Footer.tsx
+++ src/organisms/Footer.tsx
@@ -1,6 +1,11 @@
 import { ComponentChildren, JSX } from "preact";
+import { Action } from "../atoms/Action.tsx";
 
 export interface FooterProps extends JSX.HTMLAttributes<HTMLElement> {
+  companyDescription?: string;
+
+  companyName: string;
+
   nav?: ComponentChildren;
 }
 
@@ -10,15 +15,18 @@ export function Footer(props: FooterProps) {
       <div class="max-w-6xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
         <div class="md:flex justify-between">
           <div class="mb-4 md:mb-0">
-            <a href="/" class="text-2xl font-bold uppercase tracking-widest">
-              Company Name
-            </a>
-            <p class="text-gray-400 mt-2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-              nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
-              Nulla quis sem at nibh elementum imperdiet.
-            </p>
+            <Action
+              href="/"
+              class="text-2xl uppercase tracking-widest"
+            >
+              {props.companyName}
+            </Action>
+
+            {props.companyDescription && (
+              <p class="text-gray-400 mt-2">{props.companyDescription}</p>
+            )}
           </div>
+
           <div class="md:flex md:flex-col md:items-end">
             <nav class="flex flex-wrap justify-center -mx-2 mt-4 md:mt-0">
               <a
@@ -27,24 +35,28 @@ export function Footer(props: FooterProps) {
               >
                 Home
               </a>
+
               <a
                 href="#"
                 class="px-2 py-1 text-gray-400 hover:text-white md:mx-2"
               >
                 Products
               </a>
+
               <a
                 href="#"
                 class="px-2 py-1 text-gray-400 hover:text-white md:mx-2"
               >
                 Services
               </a>
+
               <a
                 href="#"
                 class="px-2 py-1 text-gray-400 hover:text-white md:mx-2"
               >
                 About Us
               </a>
+
               <a
                 href="#"
                 class="px-2 py-1 text-gray-400 hover:text-white md:mx-2"
@@ -52,10 +64,11 @@ export function Footer(props: FooterProps) {
                 Contact
               </a>
             </nav>
+
             <div class="mt-4 md:mt-8">
               <p class="text-gray-400">
                 &copy; {new Date().getFullYear()}{" "}
-                Company Name. All rights reserved.
+                {props.companyName}. All rights reserved.
               </p>
             </div>
           </div>
