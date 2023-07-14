e6af71d01485e28aeb5a132a9090e5815be28e5b
mcgear <michael.gearhardt@gmail.com>
2023-07-14 17:27:31 -0600

    Consolidate to classSet

--- src/molecules/Hero.tsx
+++ src/molecules/Hero.tsx
@@ -6,6 +6,7 @@ import {
   useActionChildren,
 } from "../atoms/Action.tsx";
 import { ActionGroup } from "./ActionGroup.tsx";
+import { classSet } from "../utils/jsx.tsx";
 
 export enum HeroStyleTypes {
   Rounded = 1 << 0,
@@ -28,9 +29,13 @@ export function Hero(props: HeroProps) {
   const callToAction = typeof props.callToAction === "string"
     ? <p class="text-xl max-w-lg text-blue-100">{props.callToAction}</p>
     : props.callToAction as ComponentChildren;
+
   return (
     <div
-      class="w-full flex px-8 h-96 justify-center items-center flex-col gap-8 bg-cover bg-center bg-no-repeat bg-gray-100 rounded-xl text-white"
+      class={classSet(
+        props,
+        "w-full flex px-8 h-96 justify-center items-center flex-col gap-8 bg-cover bg-center bg-no-repeat bg-gray-100 rounded-xl text-white",
+      )}
       style="background-image:linear-gradient(rgba(0, 0, 40, 0.8),rgba(0, 0, 40, 0.8)), url('/gallery/hero-bg.webp');"
     >
       <div class="space-y-4 text-center">
