e1649b524ecd176af51e9e70424b2ca269c37165
mcgear <michael.gearhardt@gmail.com>
2023-07-15 06:27:48 -0600

    Features call to action support

--- src/atoms/icons/_exports.tsx
+++ src/atoms/icons/_exports.tsx
@@ -1,3 +1,4 @@
 export * from "./Icon.tsx";
 export { ChevronDownIcon } from "./ChevronDownIcon.tsx";
+export { ChevronRightIcon } from "./ChevronRightIcon.tsx";
 export { UserIcon } from "./UserIcon.tsx";
