d55104a115fb166fa0806ef25c2002984fe94c68
mcgear <michael.gearhardt@gmail.com>
2023-07-17 16:12:28 -0600

    Menu with responsive

--- src/organisms/Header.tsx
+++ src/organisms/Header.tsx
@@ -69,23 +69,8 @@ export function Header(props: HeaderProps) {
         "bg-blue-500 sm:(flex justify-between items-center px-4 py-3)",
       )}
     >
-      <div class="flex items-center justify-between px-4 py-3 sm:p-0">
-        <div>
-          {logo || logoAction}
-        </div>
-
-        <div class="sm:hidden">
-          <Action type="button">
-            <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
-              <path
-                class="hidden"
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M4 18h16v-2H4v2ZM4 13h16v-2H4v2ZM4 6v2h16V6H4Z"
-              />
-            </svg>
-          </Action>
-        </div>
+      <div class="px-4 py-3 sm:p-0">
+        {logo || logoAction}
       </div>
 
       <ActionGroup
