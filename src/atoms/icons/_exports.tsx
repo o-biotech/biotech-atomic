4e821e886e1f965ef589b0e6f8fb5d1cf04f73eb
mcgear <michael.gearhardt@gmail.com>
2023-07-17 10:59:28 -0600

    CheckIcon

--- src/atoms/icons/_exports.tsx
+++ src/atoms/icons/_exports.tsx
@@ -1,4 +1,5 @@
 export * from "./Icon.tsx";
+export { CheckIcon } from "./CheckIcon.tsx";
 export { ChevronDownIcon } from "./ChevronDownIcon.tsx";
 export { ChevronRightIcon } from "./ChevronRightIcon.tsx";
 export { UserIcon } from "./UserIcon.tsx";
