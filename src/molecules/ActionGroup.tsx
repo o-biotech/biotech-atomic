84d6f5a5bc071eaa4fbdea8d02dd58311b0a8ba5
mcgear <michael.gearhardt@gmail.com>
2023-07-17 15:28:26 -0600

    Menu to MenuButton

--- src/molecules/ActionGroup.tsx
+++ src/molecules/ActionGroup.tsx
@@ -6,25 +6,42 @@ import {
   useActionChildren,
 } from "../atoms/Action.tsx";
 import { classSet } from "../utils/jsx.tsx";
+import { MenuButton, MenuButtonStyleTypes } from "./MenuButton.tsx";
 
 export interface ActionGroupProps extends JSX.HTMLAttributes<HTMLElement> {
   actionStyle?: ActionStyleTypes;
 
   children?: ComponentChildren | Array<ActionProps>;
+
+  responsive?: boolean;
+
+  toggleChildren?: ComponentChildren;
 }
 
 export function ActionGroup(props: ActionGroupProps) {
   const { nav, navActions } = useActionChildren(props.children);
+
+  let children: ComponentChildren = nav ||
+    navActions?.map((action) => (
+      <Action actionStyle={props.actionStyle} {...action} />
+    ));
+
+  if (props.responsive) {
+    children = (
+      <MenuButton
+        menuStyle={MenuButtonStyleTypes.Responsive}
+        toggleChildren={props.toggleChildren}
+        children={children}
+      />
+    );
+  }
 
   return (
     <nav
       {...props}
       class={classSet(props, "px-2 pt-2 pb-4 flex")}
     >
-      {nav ||
-        navActions?.map((action) => (
-          <Action actionStyle={props.actionStyle} {...action} />
-        ))}
+      {children}
     </nav>
   );
 }
