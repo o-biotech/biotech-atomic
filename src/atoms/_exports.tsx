759ddfe91bd50d1bf5db84d476db4e11e78686d7
mcgear <michael.gearhardt@gmail.com>
2023-08-16 16:45:20 -0600

    Try to change how passthrough exports are done

--- src/atoms/_exports.tsx
+++ src/atoms/_exports.tsx
@@ -1,7 +1,15 @@
-export {
+import {
   Action,
   type ActionAnchorProps,
   type ActionButtonProps,
   type ActionProps,
   ActionStyleTypes,
 } from "../src.deps.ts";
+
+export {
+  Action,
+  type ActionAnchorProps,
+  type ActionButtonProps,
+  type ActionProps,
+  ActionStyleTypes,
+};
--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,3 +1,3 @@
-export * from "https://deno.land/x/fathym_atomic@v0.0.8-integration/mod.ts";
+export * from "https://deno.land/x/fathym_atomic@v0.0.9-integration/mod.ts";
 export * from "https://esm.sh/*@preact/signals@1.1.5";
 // export * from "https://esm.sh/*@preact/signals-core@1.3.1/signals";
