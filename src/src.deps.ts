3bb196f1dc48479698ffa2f1eb570fbd298d8ab0
mcgear <michael.gearhardt@gmail.com>
2023-08-17 11:13:24 -0600

    upgrades

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,3 +1,3 @@
-export * from "https://deno.land/x/fathym_atomic@v0.0.33-integration/mod.ts";
+export * from "https://deno.land/x/fathym_atomic@v0.0.34-integration/mod.ts";
 // export * from "https://esm.sh/*@preact/signals-core@1.3.1/signals";
 export * from "https://esm.sh/*@preact/signals@1.1.5";
