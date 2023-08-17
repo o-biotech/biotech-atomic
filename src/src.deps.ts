4381334653febcd0649ee9e77b7199dc8598ec38
mcgear <michael.gearhardt@gmail.com>
2023-08-17 10:57:23 -0600

    change order

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,3 +1,3 @@
 export * from "https://deno.land/x/fathym_atomic@v0.0.33-integration/mod.ts";
-export * from "https://esm.sh/*@preact/signals@1.1.5";
 // export * from "https://esm.sh/*@preact/signals-core@1.3.1/signals";
+export * from "https://esm.sh/*@preact/signals@1.1.5";
