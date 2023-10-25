b0f66b43cc1707f8dcabbb8a5578ce274056512f
mcgear <michael.gearhardt@gmail.com>
2023-10-24 23:41:07 -0600

    Fix build.yml

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,3 +1,3 @@
 // export * from "../../../fathym-deno/atomic/mod.ts";
 
-export * from 'https://deno.land/x/fathym_atomic@v0.0.93-integration/mod.ts';
+export * from "https://deno.land/x/fathym_atomic@v0.0.93-integration/mod.ts";
