a7ab01e992548ebb16c2c1f250b90d553ba7d1e3
mcgear <michael.gearhardt@gmail.com>
2023-10-24 23:36:32 -0600

    upgrade deps

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,1 +1,3 @@
-export * from "https://deno.land/x/fathym_atomic@v0.0.90/mod.ts";
+// export * from "../../../fathym-deno/atomic/mod.ts";
+
+export * from 'https://deno.land/x/fathym_atomic@v0.0.93-integration/mod.ts';
