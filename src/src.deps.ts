e44e14bcf75ab44e61e0ded2c33cf05c5dfc23c4
mcgear <michael.gearhardt@gmail.com>
2023-12-18 13:00:50 -0700

    updates to packages

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,5 +1,5 @@
 // export * from "../../../fathym-deno/atomic/mod.ts";
 // export * from "../../../fathym-deno/atomic-iot/mod.ts";
 
-export * from 'https://deno.land/x/fathym_atomic@v0.0.93-integration/mod.ts';
+export * from 'https://deno.land/x/fathym_atomic@v0.0.94-integration/mod.ts';
 export * from 'https://deno.land/x/fathym_atomic_iot@v0.0.36/mod.ts';
