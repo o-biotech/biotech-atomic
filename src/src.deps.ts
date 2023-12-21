f0d146d9d46510fd725d41edc01432245bfa6145
mcgear <michael.gearhardt@gmail.com>
2023-12-21 13:04:02 -0700

    ref updates

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,5 +1,5 @@
 // export * from "../../../fathym-deno/atomic/mod.ts";
 // export * from "../../../fathym-deno/atomic-iot/mod.ts";
 
-export * from 'https://deno.land/x/fathym_atomic_design_kit@v0.0.20-integration/mod.ts';
+export * from 'https://deno.land/x/fathym_atomic_design_kit@v0.0.21-integration/mod.ts';
 export * from 'https://deno.land/x/fathym_atomic_iot@v0.0.36/mod.ts';
