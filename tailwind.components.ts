ae2c7676ae465a5fcb42477f1b54afa97757fd49
mcgear <michael.gearhardt@gmail.com>
2024-01-08 18:10:08 -0700

    tailwind components

---
+++ tailwind.components.ts
@@ -0,0 +1,12 @@
+export default {
+	Root: import.meta.resolve("./"),
+	Components: [
+		"./src/atoms/forms/_exports.tsx",
+		"./src/atoms/_exports.tsx",
+		"./src/molecules/_exports.tsx",
+		"./src/organisms/_exports.tsx",
+		"./src/state-flow/StateFlowContext.tsx",
+		"./src/templates/_exports.tsx",
+		"./src/utils/_exports.tsx"
+	],
+};
