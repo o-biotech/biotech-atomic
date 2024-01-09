dd84395c784eefabf67deaeb04046be2f1d2cf38
mcgear <michael.gearhardt@gmail.com>
2024-01-08 22:11:31 -0700

    Try this for tailwind

--- tailwind-components.config.ts
+++ tailwind-components.config.ts
@@ -1,8 +1,20 @@
 import { constructTailwindComponentsConfig } from './src/src.deps.ts';
 
-await constructTailwindComponentsConfig(import.meta, [
-  {
-    Directory: './src',
-    Extensions: ['.tsx'],
-  },
-]);
+await constructTailwindComponentsConfig(
+  import.meta,
+  [
+    {
+      Directory: './src',
+      Extensions: ['.tsx'],
+    },
+  ],
+  [
+    {
+      Root: 'https://raw.githubusercontent.com/fathym-deno/atomic/integration/',
+      Components: [
+        './src/organisms/Features.tsx?source',
+        './src/organisms/StepsFeatures.tsx?source',
+      ],
+    },
+  ]
+);
--- tailwind.components.ts
+++ tailwind.components.ts
@@ -1,12 +1,21 @@
-export default {
-  	Root: import.meta.resolve("./"),
-  	Components: [
-		"./src/atoms/forms/_exports.tsx",
-		"./src/atoms/_exports.tsx",
-		"./src/molecules/_exports.tsx",
-		"./src/organisms/_exports.tsx",
-		"./src/state-flow/StateFlowContext.tsx",
-		"./src/templates/_exports.tsx",
-		"./src/utils/_exports.tsx"
-	],
-  };
+export default [
+	{
+		Root: import.meta.resolve("./"),
+		Components: [
+			"./src/atoms/forms/_exports.tsx",
+			"./src/atoms/_exports.tsx",
+			"./src/molecules/_exports.tsx",
+			"./src/organisms/_exports.tsx",
+			"./src/state-flow/StateFlowContext.tsx",
+			"./src/templates/_exports.tsx",
+			"./src/utils/_exports.tsx",
+		],
+	},
+	{
+		Root: 'https://raw.githubusercontent.com/fathym-deno/atomic/integration/',
+		Components: [
+			"./src/organisms/Features.tsx?source",
+			"./src/organisms/StepsFeatures.tsx?source",
+		],
+	},
+];
