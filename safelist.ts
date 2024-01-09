56bf988c2cbe8f6bd28895857626ebee8525a837
mcgear <michael.gearhardt@gmail.com>
2024-01-08 17:04:35 -0700

    export files

--- safelist.ts
+++ safelist.ts
@@ -1,8 +1,13 @@
 // import { safelist as AtomicIoTSafelist } from '../../../../fathym-deno/atomic-iot/src/tailwind/safelist.ts';
 
-async function getFilesList(directory: string): Promise<string[]> {
+async function getFilesList(
+  directory: string,
+  ext?: string
+): Promise<string[]> {
   const foundFiles: string[] = [];
-  for await (const fileOrFolder of Deno.readDir(directory)) {
+  for await (const fileOrFolder of Deno.readDir(
+    import.meta.resolve(directory).replace('file:///', '')
+  )) {
     if (fileOrFolder.isDirectory) {
       // If it's not ignored, recurse and search this folder for files.
       const nestedFiles = await getFilesList(
@@ -14,10 +19,10 @@ async function getFilesList(directory: string): Promise<string[]> {
       foundFiles.push(`${directory}/${fileOrFolder.name}`);
     }
   }
-  return foundFiles;
+  return foundFiles.filter((ff) => (ext ? ff.endsWith(ext) : ff));
 }
 
-const files = await getFilesList('./src');
+const files = await getFilesList('./src', '.tsx');
 console.log(files);
 
 // export const safelist = new Set([...AtomicIoTSafelist]);
