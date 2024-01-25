2725c2400a745d692c85bd7c978c00bb82139048
mcgear <michael.gearhardt@gmail.com>
2024-01-25 11:07:29 -0700

    ref updates

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,9 +1,10 @@
 // export * from '../../../fathym-deno/fathym-atomic/mod.ts';
+// import FADKTC from '../../../fathym-deno/fathym-atomic/tailwind.components.ts';
 // export * from '../../../fathym-deno/atomic-iot/mod.ts';
 
 export * from "https://deno.land/x/fathym_common@v0.0.131/mod.ts";
 
-export * from "https://deno.land/x/fathym_atomic_design_kit@v0.0.61/mod.ts";
+export * from "https://deno.land/x/fathym_atomic_design_kit@v0.0.62/mod.ts";
 
-import FADKTC from "https://deno.land/x/fathym_atomic_design_kit@v0.0.61/tailwind.components.ts";
+import FADKTC from "https://deno.land/x/fathym_atomic_design_kit@v0.0.62/tailwind.components.ts";
 export const FathymAtomicDesignKitTailwindComponents = FADKTC;
--- tailwind.components.ts
+++ tailwind.components.ts
@@ -6,13 +6,13 @@ export default [
 		],
 	},
 	{
-		Root: 'https://deno.land/x/fathym_atomic_design_kit@v0.0.61/',
+		Root: 'https://deno.land/x/fathym_atomic_design_kit@v0.0.62/',
 		Components: [
 			"./src/atoms/Logo.tsx",
 			"./src/molecules/azure/connect.form.tsx",
 			"./src/molecules/azure/existing.form.tsx",
 			"./src/molecules/eac/calz.form.tsx",
-			"./src/molecules/eac/connect.form.tsx",
+			"./src/molecules/eac/manage-cloud.form.tsx",
 			"./src/molecules/eac/manage-devops-action.form.tsx",
 			"./src/molecules/eac/manage-handler.form.tsx",
 			"./src/molecules/eac/manage-iot-device.form.tsx",
