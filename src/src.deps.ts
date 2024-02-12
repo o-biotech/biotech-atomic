5f574d0b0e1dbb8cac559e4d6588a73bf412696a
mcgear <michael.gearhardt@gmail.com>
2024-02-12 15:11:03 -0700

    ref updates with text fixes

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -4,7 +4,7 @@
 
 export * from "https://deno.land/x/fathym_common@v0.0.137/mod.ts";
 
-export * from "https://deno.land/x/fathym_atomic_design_kit@v0.0.85/mod.ts";
+export * from "https://deno.land/x/fathym_atomic_design_kit@v0.0.86/mod.ts";
 
-import FADKTC from "https://deno.land/x/fathym_atomic_design_kit@v0.0.85/tailwind.components.ts";
+import FADKTC from "https://deno.land/x/fathym_atomic_design_kit@v0.0.86/tailwind.components.ts";
 export const FathymAtomicDesignKitTailwindComponents = FADKTC;
