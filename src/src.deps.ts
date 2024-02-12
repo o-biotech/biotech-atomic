9fdaafc47e6f9f054a92dcc8e6753d24cc94f8a9
Matt <matt.jackson@fathym.com>
2024-02-12 15:07:12 -0700

    copy updates

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,10 +1,10 @@
-// export * from '../../../fathym-deno/fathym-atomic/mod.ts';
-// import FADKTC from '../../../fathym-deno/fathym-atomic/tailwind.components.ts';
+export * from '../../../fathym-deno/fathym-atomic/mod.ts';
+import FADKTC from '../../../fathym-deno/fathym-atomic/tailwind.components.ts';
 // export * from '../../../fathym-deno/atomic-iot/mod.ts';
 
 export * from "https://deno.land/x/fathym_common@v0.0.137/mod.ts";
 
-export * from "https://deno.land/x/fathym_atomic_design_kit@v0.0.85/mod.ts";
+// export * from "https://deno.land/x/fathym_atomic_design_kit@v0.0.85/mod.ts";
 
-import FADKTC from "https://deno.land/x/fathym_atomic_design_kit@v0.0.85/tailwind.components.ts";
+// import FADKTC from "https://deno.land/x/fathym_atomic_design_kit@v0.0.85/tailwind.components.ts";
 export const FathymAtomicDesignKitTailwindComponents = FADKTC;
