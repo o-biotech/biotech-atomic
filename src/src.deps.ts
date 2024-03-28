255498ecdfb12ecb797fa4ee36a70fcd96359544
mcgear <michael.gearhardt@gmail.com>
2024-03-28 11:34:12 -0600

    ref updates

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,7 +1,5 @@
 // export * from '../../../fathym-deno/fathym-atomic/mod.ts';
-// import FADKTC from '../../../fathym-deno/fathym-atomic/tailwind.components.ts';
-// export * from '../../../fathym-deno/atomic-iot/mod.ts';
 
 export * from "https://deno.land/x/fathym_common@v0.0.177/mod.ts";
 
-export * from "https://deno.land/x/fathym_atomic_design_kit@v0.0.114/mod.ts";
+export * from "https://deno.land/x/fathym_atomic_design_kit@v0.0.115/mod.ts";
