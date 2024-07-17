fd2f8135ed0d2ea94ab4404553a31b719af205f4
mcgear <michael.gearhardt@gmail.com>
2024-07-17 09:39:05 -0600

    ref updates

--- tests/test.deps.ts
+++ tests/test.deps.ts
@@ -1,11 +1,8 @@
-export {
-  assert,
-  assertEquals,
-} from "https://deno.land/std@0.203.0/testing/asserts.ts";
+export * from 'https://deno.land/std@0.220.1/assert/mod.ts';
 export {
   afterEach,
   beforeEach,
   describe,
   it,
-} from "https://deno.land/std@0.203.0/testing/bdd.ts";
-export { render } from "preact-render-to-string";
+} from 'https://deno.land/std@0.203.0/testing/bdd.ts';
+export { render } from 'preact-render-to-string';
