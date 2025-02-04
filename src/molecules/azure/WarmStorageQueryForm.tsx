04740d02cdd063c4b96c1d5bd4955d510895a1e9
Skeletor11 <kevinbowersdeveloper@gmail.com>
2025-02-04 14:19:59 -0700

    debug 17

--- src/molecules/azure/WarmStorageQueryForm.tsx
+++ src/molecules/azure/WarmStorageQueryForm.tsx
@@ -1,8 +1,8 @@
 import {
-  //Action,
-  //ActionGroup,
-  //classSet,
-  //Input,
+  Action,
+  ActionGroup,
+  classSet,
+  Input,
   //useState,
   type JSX,
 } from "../../src.deps.ts";
@@ -58,6 +58,55 @@ export default function WarmStorageQueryForm(
  // };
 
   return (
-     <h2>TESTING</h2>
+      <form
+        method="post"
+        //{...props}
+        //class={classSet(
+        //  ["-:w-full -:max-w-sm -:md:max-w-md -:mx-auto -:p-3 -:mt-8"],
+        //  props,
+        //)}
+      >
+        <div class="flex flex-wrap -mx-3 mb-4">
+          <div class="w-full px-3">
+            <div class="w-full p-3">
+              <label
+                for="query"
+                class="block uppercase tracking-wide font-bold mb-2 text-lg text-left"
+              >
+                Query
+              </label>
+
+              <Input
+                id="query"
+                name="query"
+                type="text"
+               // value={typeof props.query === "symbol" ? String(props.query) : props.query || ""}  // Check for Symbol and handle accordingly
+                multiline
+                required
+                placeholder="Enter warm storage query"
+              />
+            </div>
+          </div>
+        </div>
+
+        <ActionGroup class="mt-8 flex-col">
+          <Action
+            type="submit"
+            class="w-full md:w-auto text-white font-bold m-1 py-2 px-4 rounded focus:outline-none shadow-lg"
+          >
+            Run
+          </Action>
+          {/* Button to open the modal */}
+          <button
+            type="button"
+            //onClick={toggleModal}
+            class="w-full md:w-auto text-blue-500 font-bold m-1 py-2 px-4 rounded focus:outline-none shadow-lg"
+          >
+            Open Modal
+          </button>
+        </ActionGroup>
+      </form>
+
+     
   );
 }
