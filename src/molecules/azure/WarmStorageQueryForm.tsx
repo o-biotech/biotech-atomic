82bc356a2d0f05615e673d0614afe73892c36730
Skeletor11 <kevinbowersdeveloper@gmail.com>
2025-02-04 13:50:46 -0700

    debug 7

--- src/molecules/azure/WarmStorageQueryForm.tsx
+++ src/molecules/azure/WarmStorageQueryForm.tsx
@@ -61,10 +61,10 @@ export default function WarmStorageQueryForm(
     <>
       <form
         method="post"
-        {...props}
+        //{...props}
         class={classSet(
           ["-:w-full -:max-w-sm -:md:max-w-md -:mx-auto -:p-3 -:mt-8"],
-          props,
+        //  props,
         )}
       >
         <div class="flex flex-wrap -mx-3 mb-4">
